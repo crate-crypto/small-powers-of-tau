0e61f6a7eb5d61aea3f8ca80b956a8d839cf83d3
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-05-08 19:28:48 +0100

    mosidy serialisation to optionally include the subgroup checks. Participants will now do a partial subgroup check, while the verifier will do a full subgroup check

--- src/serialisation.rs
+++ src/serialisation.rs
@@ -2,46 +2,110 @@ use crate::{
     accumulator::{Accumulator, Parameters},
     update_proof::UpdateProof,
 };
-use ark_bn254::{G1Projective, G2Projective};
+use ark_bls12_381::{G1Affine, G1Projective, G2Affine, G2Projective};
+use ark_ec::AffineCurve;
 use ark_ff::Zero;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 // TODO: Once we specify how to deal with failure cases, make these methods return a Result
-
+pub enum SubgroupCheck {
+    // This is very expensive, each group element is multiplied by the order of the prime subgroup
+    // The result is checked to be the identity.
+    Full,
+    // Only the first group element is checked. This should only be done for participants of the ceremony.
+    Partial,
+}
 impl Accumulator {
     pub fn serialise(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
 
         for g1 in &self.tau_g1 {
-            g1.serialize_uncompressed(&mut bytes).unwrap()
+            g1.serialize_unchecked(&mut bytes).unwrap()
         }
         for g2 in &self.tau_g2 {
-            g2.serialize_uncompressed(&mut bytes).unwrap()
+            g2.serialize_unchecked(&mut bytes).unwrap()
         }
 
         bytes
     }
+
     // TODO: modify this method to return a result.
     // TODO: what does a contributor do when they are given a zero element?
-    pub fn deserialise(bytes: &[u8], parameters: Parameters) -> Self {
-        let mut acc = Accumulator::new(parameters);
+    pub fn deserialise(
+        bytes: &[u8],
+        parameters: Parameters,
+        subgroup_check: SubgroupCheck,
+    ) -> Self {
+        // TODO: We need to deserialise into affine representation because arkworks does not have `is_on_curve` and `subgroup_check` for Projective representation
+        let mut g1 = vec![G1Affine::prime_subgroup_generator(); parameters.num_g1_elements_needed];
+        let mut g2 = vec![G2Affine::prime_subgroup_generator(); parameters.num_g2_elements_needed];
 
         let mut reader = std::io::Cursor::new(bytes);
 
-        for g1 in acc.tau_g1.iter_mut() {
-            *g1 = CanonicalDeserialize::deserialize_uncompressed(&mut reader).unwrap();
-            if g1.is_zero() {
+        for element in g1.iter_mut() {
+            *element = CanonicalDeserialize::deserialize_unchecked(&mut reader).unwrap();
+            if element.is_zero() {
                 panic!("unexpected zero point")
             }
         }
-        for g2 in acc.tau_g2.iter_mut() {
-            *g2 = CanonicalDeserialize::deserialize_uncompressed(&mut reader).unwrap();
-            if g2.is_zero() {
+        for element in g2.iter_mut() {
+            *element = CanonicalDeserialize::deserialize_unchecked(&mut reader).unwrap();
+            if element.is_zero() {
                 panic!("unexpected zero point")
+            }
+        }
+        // Check if points are on the curve
+        for element in &g1 {
+            if !element.is_on_curve() {
+                panic!("point is not on curve")
+            }
+        }
+        for element in &g2 {
+            if !element.is_on_curve() {
+                panic!("point is not on curve")
             }
         }
 
-        acc
+        // This method should be used by participants, we only check that one element is in the group
+        // then by induction and the structure of the SRS, the rest of the elements are in the group
+        //
+        // We actually make the check cheaper because we use the canonical generators,
+        // so its just an equals check
+        // TODO: write a proof for this -- assume it is unsafe
+        let first_g1_element = g1[0];
+        if first_g1_element != G1Affine::prime_subgroup_generator() {
+            panic!("first g1 element is not the canonical prime subgroup generator")
+        }
+        let first_g2_element = g2[0];
+        if first_g2_element != G2Affine::prime_subgroup_generator() {
+            panic!("first g2 element is not the canonical prime subgroup generator")
+        }
+
+        if let SubgroupCheck::Full = subgroup_check {
+            // TODO: swap this out by using the bowe endomorphism
+            g1.par_iter().for_each(|element| {
+                if !element.is_in_correct_subgroup_assuming_on_curve() {
+                    panic!("point is not in the prime subgroup")
+                }
+            });
+            g2.par_iter().for_each(|element| {
+                if !element.is_in_correct_subgroup_assuming_on_curve() {
+                    panic!("point is not in the prime subgroup")
+                }
+            });
+        }
+
+        Accumulator {
+            tau_g1: g1
+                .into_iter()
+                .map(|element| element.into_projective())
+                .collect(),
+            tau_g2: g2
+                .into_iter()
+                .map(|element| element.into_projective())
+                .collect(),
+        }
     }
 }
 
@@ -100,12 +164,11 @@ impl UpdateProof {
 #[cfg(test)]
 mod tests {
     use crate::keypair::PrivateKey;
-    use ark_bn254::Fr;
+    use ark_bls12_381::Fr;
     use ark_ec::ProjectiveCurve;
     use ark_ff::PrimeField;
 
     use super::*;
-
     #[test]
     fn update_proof_serialise_roundtrip() {
         let proof = UpdateProof {
@@ -135,7 +198,7 @@ mod tests {
         acc.update(secret);
 
         let bytes = acc.serialise();
-        let deserialised_acc = Accumulator::deserialise(&bytes, params);
+        let deserialised_acc = Accumulator::deserialise(&bytes, params, SubgroupCheck::Full);
 
         assert_eq!(acc, deserialised_acc);
     }
