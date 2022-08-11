2205a4644bf065b31bb025b7cfe744e899b7a60c
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-08-11 20:46:51 +0100

    - modify deserialisation methods in updateproof and srs to match this - remove ark-serialise

--- src/serialisation.rs
+++ src/serialisation.rs
@@ -1,4 +1,4 @@
-use crate::interop_point_encoding;
+use crate::interop_point_encoding::{deserialize_g1, deserialize_g2, serialize_g1, serialize_g2};
 use crate::{
     srs::{Parameters, SRS},
     update_proof::UpdateProof,
@@ -6,7 +6,9 @@ use crate::{
 use ark_bls12_381::{G1Affine, G1Projective, G2Affine, G2Projective};
 use ark_ec::{AffineCurve, ProjectiveCurve};
 use ark_ff::Zero;
-use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Read};
+use std::io::Read;
+
+// TODO: use JSON serialisation strategy that is being used in the python specs
 
 impl SRS {
     pub fn serialise(&self) -> Vec<u8> {
@@ -16,12 +18,12 @@ impl SRS {
         let g2_points_affine = G2Projective::batch_normalization_into_affine(&self.tau_g2);
 
         for point in &g1_points_affine {
-            let points_as_bytes = interop_point_encoding::serialize_g1(point);
+            let points_as_bytes = serialize_g1(point);
             bytes.extend(points_as_bytes);
         }
 
         for point in &g2_points_affine {
-            let points_as_bytes = interop_point_encoding::serialize_g2(point);
+            let points_as_bytes = serialize_g2(point);
             bytes.extend(points_as_bytes);
         }
 
@@ -31,7 +33,6 @@ impl SRS {
     // We do not check if the point is the identity when deserialising
     // What we do check, is that every point is a point on the curve
     pub fn deserialise(bytes: &[u8], parameters: Parameters) -> Result<Self, ()> {
-        use crate::interop_point_encoding::{deserialize_g1, deserialize_g2};
         // TODO: We need to deserialise into affine representation because arkworks does not have `is_on_curve` and `subgroup_check` for Projective representation
         // TODO: its possible to deserialise and then do the checks first, then store it in the
         // TODO: vector
@@ -39,17 +40,22 @@ impl SRS {
         let mut g2 = vec![G2Affine::prime_subgroup_generator(); parameters.num_g2_elements_needed];
 
         let mut reader = std::io::Cursor::new(bytes);
-        // TODO: note when we deserialise, we get the y coordinate using the x
-        // TODO: coordinate, so we do not need to do a subgroup check
+
         for element in g1.iter_mut() {
             let mut point_bytes = [0u8; 48];
             reader.read_exact(&mut point_bytes).unwrap();
-            *element = deserialize_g1(point_bytes);
+            match deserialize_g1(point_bytes) {
+                Some(des_point) => *element = des_point,
+                None => return Err(()),
+            };
         }
         for element in g2.iter_mut() {
             let mut point_bytes = [0u8; 96];
             reader.read_exact(&mut point_bytes).unwrap();
-            *element = deserialize_g2(point_bytes);
+            match deserialize_g2(point_bytes) {
+                Some(des_point) => *element = des_point,
+                None => return Err(()),
+            };
         }
 
         Ok(SRS {
@@ -68,23 +74,26 @@ impl SRS {
 impl UpdateProof {
     pub fn serialise(&self) -> Vec<u8> {
         let mut bytes = Vec::new();
+        let public_key_bytes = serialize_g2(&self.commitment_to_secret.into_affine());
+        let update_point_bytes = serialize_g1(&self.new_accumulated_point.into_affine());
 
-        self.commitment_to_secret
-            .serialize_uncompressed(&mut bytes)
-            .unwrap();
-        self.new_accumulated_point
-            .serialize_uncompressed(&mut bytes)
-            .unwrap();
+        bytes.extend(public_key_bytes);
+        bytes.extend(update_point_bytes);
 
         bytes
     }
     pub fn deserialise(bytes: &[u8]) -> Self {
         let mut reader = std::io::Cursor::new(bytes);
 
-        let commitment_to_secret: G2Projective =
-            CanonicalDeserialize::deserialize_uncompressed(&mut reader).unwrap();
-        let new_accumulated_point: G1Projective =
-            CanonicalDeserialize::deserialize_uncompressed(&mut reader).unwrap();
+        let mut point_bytes = [0u8; 96];
+        reader.read_exact(&mut point_bytes).unwrap();
+        let commitment_to_secret = deserialize_g2(point_bytes).unwrap();
+        let commitment_to_secret = commitment_to_secret.into_projective();
+
+        let mut point_bytes = [0u8; 48];
+        reader.read_exact(&mut point_bytes).unwrap();
+        let new_accumulated_point = deserialize_g1(point_bytes).unwrap();
+        let new_accumulated_point = new_accumulated_point.into_projective();
 
         assert!(!commitment_to_secret.is_zero());
         assert!(!new_accumulated_point.is_zero());
