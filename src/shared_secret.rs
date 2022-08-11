3b5e106a662a57d85f5acfba77de6fcf6bb8709a
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-08-11 22:50:14 +0100

    add test for shared secret

--- src/shared_secret.rs
+++ src/shared_secret.rs
@@ -26,6 +26,11 @@ impl SharedSecretChain {
         self.accumulated_points.push(new_accumulated_point);
         self.witnesses.push(witness)
     }
+    #[cfg(test)]
+    fn remove_last(&mut self) {
+        self.accumulated_points.pop();
+        self.witnesses.pop();
+    }
 
     // Verifies a shared secret chain, each srs is checked to have been transformed from the previous one
     // using the specified witness
@@ -46,5 +51,63 @@ impl SharedSecretChain {
             }
         }
         true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use ark_bls12_381::{Fr, G1Projective, G2Projective};
+    use ark_ec::ProjectiveCurve;
+    use ark_ff::PrimeField;
+
+    use super::SharedSecretChain;
+
+    #[test]
+    fn shared_secret_smoke() {
+        let g1_generator = G1Projective::prime_subgroup_generator();
+        let g2_generator = G2Projective::prime_subgroup_generator();
+
+        let a = Fr::from(20u64);
+        let b = Fr::from(21u64);
+        let c = Fr::from(23u64);
+        let d = Fr::from(24u64);
+
+        let a_witness = g2_generator.mul(a.into_repr());
+        let b_witness = g2_generator.mul(b.into_repr());
+        let c_witness = g2_generator.mul(c.into_repr());
+        let d_witness = g2_generator.mul(d.into_repr());
+
+        let mut chain = SharedSecretChain::starting_from(g1_generator);
+
+        // Add `a` into the product so we have `a * G1` as the accumulated point
+        //
+        let a_g1 = g1_generator.mul(a.into_repr());
+        chain.extend(a_g1, a_witness);
+        assert!(chain.verify());
+        //
+        // Add `b` into the product so we have `a * b * G1` as the accumulated point
+        let ab_g1 = a_g1.mul(b.into_repr());
+        chain.extend(ab_g1, b_witness);
+        assert!(chain.verify());
+        //
+        // Add `c` into the product so we have `a * b * c * G1` as the accumulated point
+        let abc_g1 = ab_g1.mul(c.into_repr());
+        chain.extend(abc_g1, c_witness);
+        assert!(chain.verify());
+        //
+        // Add `d` into the product so we have `a * b * c * d * G1` as the accumulated point
+        let abcd_g1 = abc_g1.mul(d.into_repr());
+
+        // -- Now for the witness we add `c` instead of `d`. This should verify as false
+        chain.extend(abcd_g1, c_witness);
+        assert!(chain.verify() == false);
+        chain.remove_last();
+        // -- Now add the correct witness, but the wrong accumulated point
+        chain.extend(abc_g1, d_witness);
+        assert!(chain.verify() == false);
+        chain.remove_last();
+        // -- Add the correct accumulated point and witness
+        chain.extend(abcd_g1, d_witness);
+        assert!(chain.verify())
     }
 }
